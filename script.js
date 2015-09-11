6abc17fab51a0e3cc15ea14a6e2d16f01520e811
unknown <lookmail88@gmail.com>
2015-09-10 21:54:53 -0700

    second Commit

---
+++ script.js
@@ -0,0 +1,53 @@
+/*
+ *   Author:Xuyang Gao
+ *   Date:9/10/2015
+ */
+
+//step 1
+//var someMonth;
+//function theMonth 		// function to return current month
+//currentMonth 		// a constant
+//var summerMonth; 		// an array of summer months
+//myLibraryAFunction 	// a function 
+
+
+
+//step2
+//var numb=1.0;
+//var str="Xuyang Gao";
+//var isGoodPerson=true;
+//var score=null;
+
+//step3
+//var sample1=3*4/5;
+//var sample2="Hello"+" "+"World"+"!";
+
+//step4
+//var strFirstName;
+//var strLastName; 
+//var strAddress;
+//var strCity;
+//var strState
+//var strZipCode;
+//var numbYourAge;
+//var strReferral;
+//var strSource;
+//var booleanMayWeContactYou;
+
+//step5
+//var strFirstName="Xuyang";
+//var strLastName="Gao"; 
+//var strAddress="8355 Areo street, San Diego, CA 92129";
+
+//step6
+//var getYear="This year is "+2015;
+//console.log(getYear);
+//var getGender="Your gender is male. It is "+true;
+//console.log(getGender);
+//var getResult=0+true;
+//console.log(getResult);
+
+//step7
+carNumb=7;
+console.log("There are "+carNumb+" cars here.");
+var carNumb;

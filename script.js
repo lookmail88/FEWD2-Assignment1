74b3b359e3f7947e41030767efe80459148677d4
unknown <lookmail88@gmail.com>
2015-09-15 20:33:47 -0700

    Assignment finished

--- script.js
+++ script.js
@@ -48,6 +48,99 @@
 //console.log(getResult);
 
 //step7
-carNumb=7;
-console.log("There are "+carNumb+" cars here.");
-var carNumb;
+//carNumb=7;
+//console.log("There are "+carNumb+" cars here.");
+//var carNumb;
+
+//step8
+//var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+//console.log(someString);
+
+//step9
+//var car=null;
+//console.log(car);
+//var car2;
+//console.log(car2);
+
+//step10
+//var var1="";
+//var var2=10;
+//var var3=false;
+//var var4={"lastname":"Gao","firstname":"Xuyang"};
+//var var5;
+//console.log(typeof(var1)+","+typeof(var2)+","+typeof(var3)+","+typeof(var4)+","+typeof(var5));
+
+//step11
+//alert("Hello "+"Xuyang Gao"+", welcome to the JavaScript class!");
+
+//step12
+//var person="Xuyang Gao"
+//alert("Hello "+person+", welcome to the JavaScript class!");
+
+//step13
+//var course = "JavaScript";
+//var person="Xuyang Gao"
+//alert("Hello "+person+", welcome to the "+course+" class!");
+
+//step14
+//var course = "JavaScript";
+//var person="Xuyang Gao"
+//alert("Hello "+person+".\nWelcome to the "+course+" class!");
+
+//step15
+//var course = "JavaScript";
+//var person=prompt("Please input your name.");
+//alert("Hello "+person+".\nWelcome to the "+course+" class!");
+
+//step15
+//var course = prompt("Please input your course's name.");
+//var person=prompt("Please input your name.");
+//alert("Hello "+person+".\nWelcome to the "+course+" class!");
+
+//step17
+//var x=10;
+//var y=20;
+//console.log(x+y);
+
+//step18
+//var x=20;
+//x=x+20;
+//console.log(x);
+
+//step19
+//var x=20;
+//x=x*5;
+//console.log(x);
+
+//step20
+//var x=20;
+//x=x%3;
+//console.log(x);
+
+//step21
+//var x=1;
+//var y=2;
+//var z=3;
+//var result=z>x&&z>y ;
+//console.log("x="+x+" y="+y+" z="+z);
+//console.log("Is z>x and z>y? Result=" + result);
+
+//step22
+//var x=1;
+//var y=2;
+//var z=3;
+//var result=z>x||z<y ;
+//console.log("x="+x+" y="+y+" z="+z);
+//console.log("Is z>x or z<y? Result=" + result);
+
+//step23
+//var widget=new String();
+//console.log(typeof(widget));
+
+//step24
+//var widget=new String();
+//console.log(widget instanceof String);
+
+//step25
+//var widget=new String();
+//console.log(widget instanceof Date);
